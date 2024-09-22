f47a195893a94394da68098b8e2fd7a7af854fa4
Valdes Chris <valdesfeutseu@gmail.com>
2024-09-22 21:06:25 +0100

    Accueil.v1.1

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -153,28 +153,20 @@ export default function NavBar() {
                             <div class="nav-language">
                               FR
                             </div>
-                            <span class="text-emerald-300">
-                              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
-                                <path fillRule="evenodd" d="M12.53 16.28a.75.75 0 0 1-1.06 0l-7.5-7.5a.75.75 0 0 1 1.06-1.06L12 14.69l6.97-6.97a.75.75 0 1 1 1.06 1.06l-7.5 7.5Z" clipRule="evenodd" />
-                              </svg>
-
-                            </span>
                           </div>
                         </option>
                       </select>
                     </div>
 
-                    <div className=" flex items-center justify-center bg-green-color btn-nav">
-                      <Link to='/' className="btn-nav-text" href="#">
-                        Connexion
+                    <div className="flex items-center justify-center gap-x-2">
+                      <Link to='/' className="btn-nav-text  flex items-center justify-center bg-green-color btn-nav" href="#">
+                        <span>Connexion</span>
+                        <Arrow />
                       </Link>
-                      <Arrow />
-                    </div>
-                    <div className=" flex items-center justify-center bg-green-deep btn-nav">
-                      <Link to="/" className="btn-nav-text" href="#">
-                        Contacter
+                      <Link to="/" className="btn-nav-text flex items-center justify-center bg-green-deep btn-nav" href="#">
+                        <span>Contacter</span>
+                        <Message />
                       </Link>
-                      <Message />
                     </div>
                   </div>
                 </div>
