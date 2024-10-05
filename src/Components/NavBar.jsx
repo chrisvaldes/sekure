118231a442104f5d9ad9fd6a416992d4c45e6ec2
Valdes Chris <valdesfeutseu@gmail.com>
2024-10-05 22:13:21 +0100

    responsif

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -53,11 +53,19 @@ export default function NavBar() {
               </div>
 
               <div className="md:flex md:items-center md:gap-12">
-                <a className="block" href="#">
+              <div className="menu-bars-md" ref={menuButtonRef} onClick={toggleSidebar}>
+                <button className="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="size-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+                  </svg>
+                </button>
+              </div>
+              
+                <div className="block">
                   <div className="logo flex items-center justify-center">
                     <img src={logo} alt="" />
                   </div>
-                </a>
+                </div>
               </div>
 
               <div className="nav-items">
